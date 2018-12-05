15ec024abef48ffea473946d834e015799b7b3c3
Colby Morrison <colbyamorrison@gmail.com>
2018-12-04 20:09:49 -0500

    deleted old method

--- my_unzip.c
+++ my_unzip.c
@@ -6,38 +6,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-int decode(char *in, int len){
-    int count = 0;
-    //iterate over compressed text input
-    for (int i = 0; i < len; i++)
-    {
-	//cursor    
-        char cur = in[i];
-        int code = (int) cur;
-        printf("%d: %d\n", i, code);
-	//if the character is a digit: parse full digit into count
-        if ((i + 1) % 5 != 0){
-            
-	    //if it is a digit, the last count has an extra order of magnitude to account for before adding the new low order value
-            count += code;
-	}
-	//otherwise it is the letter to print
-        else{
-	   while(count > 0){
-           if(code > 0)
-	        printf("%c",cur);
-	      count--;	      	   
-	   }
-	   //reset count inside else block after loop to protect from errors
-           count = 0;
-       	} 
-    }
-   return 0;
-}
-
-
-
-
 
 int main(int argc, char *argv[]) {
     FILE *fp = fopen(argv[1], "rb");
@@ -64,11 +32,8 @@ int main(int argc, char *argv[]) {
             fclose(fp);
             return 1;
         }
-        for(int i = 0; i < num; i++){
+        for(int i = 0; i < num; i++)
             printf("%c", asc); 
-        }
-         // Decompress file
-       // decode(contents, length);
     }
     fclose(fp);
     return 0;
