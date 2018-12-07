3d5dd63c8be050a53df6b085708ab4cf52581cd1
colbymorrison <colbyacts@gmail.com>
2018-12-07 16:59:31 -0500

    work on producer consumer

--- pzip.c
+++ pzip.c
@@ -7,38 +7,20 @@
 #include <string.h>
 #include <sys/sysinfo.h>
 #include <pthread.h>
+#include <sys/mman.h>
 
-int parse(char *in)
-{
-    char run = in[0];
+int chunk_size = 2 << 12;
+int buf_size = 0;
+int use = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t fill = PTHREAD_COND_INITIALIZER;
+pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
+
+void *pparse(char *chunk){
     long count = 0;
-    for (int i = 0; i < strlen(in); i++)
-    {
-        char cur = in[i];
-        if (cur == run)
-            count++;
-        else
-        {
-            fwrite(&count, sizeof(count), 1, stdout);
-            fwrite(&run, sizeof(char), 1, stdout);
-            run = cur;
-            count = 1;
-        }
-    }
-    fwrite(&count, sizeof(count), 1, stdout);
-    fwrite(&run, sizeof(char), 1, stdout);
-    return 0;
-}
-
-typedef struct __chunk{
-    int size;
-    char *contents;
-} chunk;
-
-void *pparse(void *in){
-    chunk *my_chunk = (chunk *) in;
-    for(int i = 0; i < my_chunk->size; i++){
-        char cur = in[i];
+    char run = chunk[0];
+    for(int i = 0; i < chunk_size; i++){
+        char cur = chunk[i];
         if (cur == run)
             count++;
         else
@@ -54,11 +36,28 @@ void *pparse(void *in){
     return 0;
 }
 
+void *consume(void *chunks){
+    for(int i = 0; i < buf_size; i ++){
+        int lock = pthread_mutex_lock(&mutex);
+        if(lock){
+            print("Lock error");
+            return 1;
+        }
+
+        int my_use = use;
+        if(use == buf_size)
+            return 0;
+        char *chunk = (char *)chunks + chunk_size*my_use;
+        use++;
+        pthread_cond_signal(&empty);
+        pthread_mutex_unlock(&mutex);
+        pparse(chunk);
+    }
+}
 int main(int argc, char *argv[])
 {
     //get_nprocs_conf = how many procs configured, nonconf is available
     long numproc = get_nprocs();
-    long chunk_size = 2^12;
     FILE *fp = fopen(argv[1], "r");
     if(!fp){
         fclose(fp);
@@ -74,29 +73,32 @@ int main(int argc, char *argv[])
     fclose(fp);
 
     pthread_t rope[numproc];
-    int chunk_count[length/chunk_size] = {0};
     //have to def outside so we can join later on # of threads created,
     // not # of threads we SHOULD have created
     int iter;
+    char **buf = (char **) malloc(length/chunk_size * sizeof(char *)); // CHECK MALLOC ERROR
     // Initial threads
     for(iter = 0; iter < numproc; iter++){
-        if (pthread_create(&rope[iter], NULL, pparse, contents + (chunk_size * iter)!= 0){
+        if (pthread_create(&rope[iter], NULL, consume, contents)!= 0){
 		    fprintf(stderr, "Failed to create thread number %d", iter);
 		    break;
 	    }
-        chunk_count[iter] = 1;
     }
 
+
     // THIS HERE IS PRODUCER CONSUMER SO LETS DO THAT YA WHOo
-    for (int i = 0; i < iter; i++){
-            //not sure, but if not null for arg2 we could track the return
-	    //values of EACH thread by storing in an array? idk why we would
-	    //need to though for this project
-	    if (pthread_join(rope[i], NULL) != 0){
-		    fprintf(stderr, "Failed to join with thread number %d", i);
-		    break;
-	    }
-    }
+
+
+
+    // for (int i = 0; i < iter; i++){
+    //         //not sure, but if not null for arg2 we could track the return
+	//     //values of EACH thread by storing in an array? idk why we would
+	//     //need to though for this project
+	//     if (pthread_join(rope[i], NULL) != 0){
+	// 	    fprintf(stderr, "Failed to join with thread number %d", i);
+	// 	    break;
+	//     }
+    // }
 
     //parse(contents);
     // Free memory
