4b837ea3b39e79a71ced19c080f70c5e1d773756
Colby Morrison <colbyamorrison@gmail.com>
2018-12-09 16:44:57 -0500

    Consumer producer

--- pzip.c
+++ pzip.c
@@ -10,13 +10,18 @@
 #include <sys/mman.h>
 
 int chunk_size = 2 << 12;
-int buf_size = 0;
+char **chunks;
+int fill = 0;
 int use = 0;
+int count = 0;
+long num_chunks = 0;
+int err = 1;
+int good = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t fill = PTHREAD_COND_INITIALIZER;
+pthread_cond_t full = PTHREAD_COND_INITIALIZER;
 pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
 
-void *pparse(char *chunk){
+char *pparse(char *chunk){
     long count = 0;
     char run = chunk[0];
     for(int i = 0; i < chunk_size; i++){
@@ -36,24 +41,38 @@ void *pparse(char *chunk){
     return 0;
 }
 
-void *consume(void *chunks){
-    for(int i = 0; i < buf_size; i ++){
-        int lock = pthread_mutex_lock(&mutex);
-        if(lock){
-            print("Lock error");
-            return 1;
+void *consume(void *arg){
+    for(int i = 0; i < num_chunks; i ++){
+        if(pthread_mutex_lock(&mutex)){
+            fprintf(stderr, "Lock error\n");
+            return &err;
         }
 
-        int my_use = use;
-        if(use == buf_size)
-            return 0;
-        char *chunk = (char *)chunks + chunk_size*my_use;
+        while(count == 0)
+            if(pthread_cond_wait(&full, &mutex)){
+                fprintf(stderr, " error\n");
+                return &err;
+            }
+
+        if(use == num_chunks - 1)
+            return &good; 
+
+        char *chunk = chunks[use];
         use++;
-        pthread_cond_signal(&empty);
-        pthread_mutex_unlock(&mutex);
+        count--;
+        if(pthread_cond_signal(&empty)){
+                fprintf(stderr, " error\n");
+                return &err;
+            }
+        if(pthread_mutex_unlock(&mutex)){
+                fprintf(stderr, " error\n");
+                return &err;
+            }
         pparse(chunk);
     }
+    return &good;
 }
+
 int main(int argc, char *argv[])
 {
     //get_nprocs_conf = how many procs configured, nonconf is available
@@ -68,41 +87,68 @@ int main(int argc, char *argv[])
     long length = ftell(fp);
     fseek(fp, 0, SEEK_SET);
 
-    // Map the file to address space
-    void *contents = mmap(NULL, length, PROT_READ, NULL, fp, 0);
-    fclose(fp);
+    num_chunks = length/chunk_size;
+    // // Map the file to address space
+    // void *contents = mmap(NULL, length, PROT_READ, NULL, fp, 0);
+    // fclose(fp);
 
     pthread_t rope[numproc];
+
+    // Create buffer, calloc to initalize to zero
+    chunks = (char **) malloc(num_chunks * sizeof(char *)); // CHECK MALLOC ERROR
+    if(!chunks){
+        fprintf(stderr, "Malloc error");
+        return 1;
+    }
+
     //have to def outside so we can join later on # of threads created,
     // not # of threads we SHOULD have created
     int iter;
-    char **buf = (char **) malloc(length/chunk_size * sizeof(char *)); // CHECK MALLOC ERROR
-    // Initial threads
+    // Create consumers
     for(iter = 0; iter < numproc; iter++){
-        if (pthread_create(&rope[iter], NULL, consume, contents)!= 0){
+        if (pthread_create(&rope[iter], NULL, consume, NULL)){
 		    fprintf(stderr, "Failed to create thread number %d", iter);
 		    break;
 	    }
     }
 
+    // "Producer"
+    for(long i = 0; i < num_chunks; i++){
+        // Do this w/o lock
+        char *contents = (char *) mmap(NULL, chunk_size, PROT_READ, MAP_FILE, fileno(fp), i*chunk_size);
+        if(pthread_mutex_lock(&mutex)){
+                fprintf(stderr, " error\n");
+                return 1;
+            }
+        while(count == num_chunks)
+            if(pthread_cond_wait(&empty, &mutex)){
+                fprintf(stderr, " error\n");
+                return 1;
+            }
+        chunks[fill] = contents;
+        fill++;
+        count++;
+        if(pthread_cond_signal(&full)){
+                fprintf(stderr, " error\n");
+                return 1;
+            }
+        if(pthread_mutex_unlock(&mutex)){
+                fprintf(stderr, " error\n");
+                return 1;
+            }
+    }
 
-    // THIS HERE IS PRODUCER CONSUMER SO LETS DO THAT YA WHOo
+    for (int i = 0; i < iter; i++){
+            //not sure, but if not null for arg2 we could track the return
+	    //values of EACH thread by storing in an array? idk why we would
+	    //need to though for this project
+	    if (pthread_join(rope[i], NULL)){
+		    fprintf(stderr, "Failed to join with thread number %d", i);
+		    break;
+	    }
+    }
 
-
-
-    // for (int i = 0; i < iter; i++){
-    //         //not sure, but if not null for arg2 we could track the return
-	//     //values of EACH thread by storing in an array? idk why we would
-	//     //need to though for this project
-	//     if (pthread_join(rope[i], NULL) != 0){
-	// 	    fprintf(stderr, "Failed to join with thread number %d", i);
-	// 	    break;
-	//     }
-    // }
-
-    //parse(contents);
-    // Free memory
-    free(contents);
+    free(chunks);
     return 0;
 } 
 
