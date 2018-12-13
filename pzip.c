2bf288361c4ecad00e9f3ae597ffc612b01a5139
colbymorrison <colbyacts@gmail.com>
2018-12-13 16:27:40 -0500

    prnt

--- pzip.c
+++ pzip.c
@@ -22,8 +22,6 @@ pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
 pthread_cond_t print = PTHREAD_COND_INITIALIZER;
 
 int *pparse(char *chunk, int chunkBound){
-    //printf("Strlen %ld, first char %c\n", strlen(chunk), chunk[0]);
-
     int *out = (int *) malloc((5 * chunkBound) + 1);
     if(!out){
        printf("Malloc error"); 
@@ -33,7 +31,6 @@ int *pparse(char *chunk, int chunkBound){
     char run = chunk[0];
 
     for(long i = 0; i < chunkBound; i++){
-        // printf("Starting loop %ld, first char %c\n", i, chunk[0]);
         char cur = chunk[i];
         if (cur == run)
             my_count++;
@@ -49,7 +46,6 @@ int *pparse(char *chunk, int chunkBound){
     out[my_fill + 1] = run;
     out[my_fill + 2] = -1; 
     munmap(chunk, chunkBound);
-   // printf("We have processed, first char: %c\n", chunk[0]);
     return out;
 }
 
@@ -59,7 +55,6 @@ typedef struct __dasein{
     int *valid;
     void *fp;
     long num_chunks;
-    int err;
 } dasein;
 
 //this seems highly redundant, why dont we just define a struct then malloc(sizeof(struct))?
@@ -91,25 +86,20 @@ void *producer(void *arg){
     while(1){
         if(pthread_mutex_lock(&mutex)){
             fprintf(stderr, "Lock error\n");
-            order->err = 1;
             return NULL; 
         }
         if(fill >= order->num_chunks){
             if(pthread_mutex_unlock(&mutex)){
                 fprintf(stderr, "unlock error\n");
-                order->err = 1;
                 return NULL;
             }
-            order->err = 0;
             return NULL;
         }
         int my_fill = fill;
-       // printf("My fill %d\n", my_fill);
         char *contents = (char *) mmap(NULL, chunk_size, PROT_READ, MAP_PRIVATE, fileno(order -> fp), my_fill*chunk_size);
         fill++;
         if(pthread_mutex_unlock(&mutex)){
             fprintf(stderr, "unlock error\n");
-            order->err = 1;
             return NULL;
         }
         long chunkBound = chunk_size;
@@ -121,37 +111,29 @@ void *producer(void *arg){
 
         if(pthread_mutex_lock(&mutex)){
             fprintf(stderr, "Lock error\n");
-            order->err = 1;
             return NULL;
         }
         order->chunks[my_fill] = parsed;
         order->valid[my_fill] = 1;
         if(pthread_cond_signal(&full)){
             fprintf(stderr, "cond signal error\n");
-            order->err = 1;
             return NULL;
         }
         if(pthread_mutex_unlock(&mutex)){
             fprintf(stderr, "unlock error\n");
-            order->err = 1;
             return NULL;
         }
-        //printf("outlock 2 looping, num_chunks: %ld\n", num_chunks);
     }
-    //printf("REturning!\n");
-    order->err = 0;
     return NULL;
 }
 
 int consumer(dasein *order){
    while(use < order->num_chunks){
-       // printf("Wait!\n");
         if(pthread_mutex_lock(&mutex)){
             fprintf(stderr, "Lock error\n");
             return 1;
         }
         while(!order->valid[use]){
-            //printf("Waiting. Use %d\n", use);
             if(pthread_cond_wait(&full, &mutex)){
                fprintf(stderr, "cond wait error\n");
                return 1;
@@ -160,7 +142,6 @@ int consumer(dasein *order){
         int *chunk = order->chunks[use]; 
         use++;
         int iter = 0;
-       // printf("Printing chunk %d\n", use);
         while(chunk[iter] != -1){
             fwrite(&chunk[iter], sizeof(int), 1, stdout);
             fwrite(&chunk[iter + 1], sizeof(char), 1, stdout);
@@ -195,7 +176,6 @@ int main(int argc, char *argv[])
     if(num_chunks < numproc){
         numproc = num_chunks;
     }
-    //printf("num chunks %ld\n", num_chunks);
 
     // Create struct
     dasein *chunkster = anxiety(num_chunks);
@@ -211,23 +191,17 @@ int main(int argc, char *argv[])
 		    fprintf(stderr, "Failed to create thread number %d", iter);
 		    break;
 	    }
-        //else printf("Created thread %d succesfully\n", iter);
     }
 
     int consume = consumer((void *)chunkster);
     if(consume)
         return 1;
   
-    // printf("Waiting to join\n");
-
     for (int i = 0; i < iter; i++){
             //not sure, but if not null for arg2 we could track the return
 	    //values of EACH thread by storing in an array? idk why we would
 	    //need to though for this project
         int ret = pthread_join(rope[i], NULL);
-        if (chunkster->err){
-            fprintf(stderr, "Producer error\n");
-        }
 	    if (ret){
 		    fprintf(stderr, "Failed to join with thread number %d\n", i);
             fprintf(stderr, "Error code: %s\n", strerror(ret));
